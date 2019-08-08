7e5fa75a57240aeacf193e55e84db45fecad7adf
Nikhil Ashodariya <nashodar@amazon.com>
2019-08-08 17:27:35 -0400

    changing response

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const app = express();
 
 app.get("/", (req, res) => {
-  res.send({ hello: "Niks rock dude" });
+  res.send({ hello: "dude is cool" });
 });
 
 const PORT = process.env.PORT || 5000;
